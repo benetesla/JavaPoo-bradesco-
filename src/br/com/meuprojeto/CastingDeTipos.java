3c143792d817b55bab618bed2af66bbe2adc4fa2
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-04-07 19:24:01 -0300

    Usando loops em nossosistema

--- src/br/com/meuprojeto/CastingDeTipos.java
+++ src/br/com/meuprojeto/CastingDeTipos.java
@@ -1,9 +1,36 @@
 package br.com.meuprojeto;
 
 public class CastingDeTipos {
-  public static void main(String[] args) {
-    double salario = 1270.50;
-    int valor = (int) salario;
-    System.out.println(valor);
-  }  
+    public static void main(String[] args) {
+        double salario = 1270.50;
+        int valor = (int) salario;
+        System.out.println(valor);
+        double d = 5.5;
+        float f = 15;
+        float x = f + (float) d;
+        System.out.println(x);
+        int idade = 15;
+        boolean AmigoDoDono = true;
+
+        if (idade < 18 && !AmigoDoDono) {
+            System.out.println("Não pode entrar");
+        } else {
+            System.out.println("Pode entrar");
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i tem o valor: " + i);
+        }
+        int i = 11;
+        while (i < 10) {
+            System.out.println("i tem o valor: " + i);
+            i++;
+        }
+        //o do while executa pelo menos uma vez
+        int j = 0;
+        do {
+            System.out.println("j tem o valor: " + j);
+            j++;
+        } while (j < 10);
+
+    }
 }
