d6547300258ce770d35a90bf86d4e2d3e955808f
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-04-07 12:40:24 -0300

    Criando o sys out do sistema

--- src/App.java
+++ src/App.java
@@ -1,50 +1,5 @@
-import br.com.meuprojeto.Funcionario;
-import br.com.meuprojeto.Cachorro;
-import br.com.meuprojeto.Gato;
 public class App {
-    public static void main(String[] args) throws Exception {
-        Cachorro cachorro = new Cachorro();
-        cachorro.setNome("Totó");
-        cachorro.setRaca("Pitbull");
-        cachorro.setIdade(2);
-        cachorro.setQuantidadeDePatas(4);
-        
-        cachorro.latir();
-        cachorro.correr();
-        System.out.println(cachorro.getNome());
-        System.out.println(cachorro.getRaca());
-        System.out.println(cachorro.getIdade());
-        System.out.println(cachorro.getQuantidadeDePatas());
-
-        //segundo cachorro
-        Cachorro cachorro2 = new Cachorro();
-        cachorro2.setNome("Apolo");
-        cachorro2.setRaca("Pitbull");
-        cachorro2.setIdade(2);
-        cachorro2.setQuantidadeDePatas(4);
-
-        cachorro2.latir();
-        cachorro2.correr();
-        System.out.println(cachorro2.getNome() + " " + cachorro2.getRaca() + " " + cachorro2.getIdade() + " " + cachorro2.getQuantidadeDePatas());
-
-        //gato
-
-        Gato gato = new Gato();
-        gato.setNome("Salem");
-        gato.setRaca("Persa");
-        gato.setIdade(2);
-        gato.setHabilidadeDePular(2);
-        gato.setHabitos(2);
-
-        gato.miar();
-        gato.correr();
-        System.out.println(gato.getNome() + " " + gato.getRaca() + " " + gato.getIdade() + " " + gato.getHabilidadeDePular() + " " + gato.getHabitos());
-    
-        //funcionario
-        Funcionario funcionario = new Funcionario();
-        funcionario.setNome("João");
-        funcionario.setAtivo(true);
-
-        System.out.println(funcionario.getNome() + " " + funcionario.isAtivo());
+    public static void main(String[] args) {
+        System.out.println("Primeiro programa Java");
     }
 }
