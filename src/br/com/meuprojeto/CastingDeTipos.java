00be0cad9c31abde10702b81587e493ff8a691bd
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-04-07 18:57:25 -0300

    Criando um sistema web

---
+++ src/br/com/meuprojeto/CastingDeTipos.java
@@ -0,0 +1,9 @@
+package br.com.meuprojeto;
+
+public class CastingDeTipos {
+  public static void main(String[] args) {
+    double salario = 1270.50;
+    int valor = (int) salario;
+    System.out.println(valor);
+  }  
+}
