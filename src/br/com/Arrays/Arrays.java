c083b4f758797074ed90667e3fa0f9bdf946b51e
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-04-08 16:12:44 -0300

    brincando com o linkelist

--- src/br/com/Arrays/Arrays.java
+++ src/br/com/Arrays/Arrays.java
@@ -1,5 +1,6 @@
 package br.com.Arrays;
 import java.util.ArrayList;
+import java.util.LinkedList;
 public class Arrays {
     public static void main(String[] args) {
         //usando o arraylist
@@ -9,20 +10,37 @@ public class Arrays {
         nomes.add("José");
         nomes.add("Pedro");
         nomes.add("Ana");
-
         //imprimindo o arraylist
         System.out.println(nomes);
         System.out.println("Tamanho do arraylist: " + nomes.size());
         System.out.println("Nome na posição 2: " + nomes.get(2));
         System.out.println("Nome na posição 3: " + nomes.get(0));
-
         //removendo um nome
-
         nomes.remove(0);
         System.out.println("Nome na posição 0: " + nomes.get(0));
-
         //removendo todos os nomes
         nomes.clear();
         System.out.println("Tamanho do arraylist: " + nomes.size());
+        //linkedList
+
+        LinkedList<String> nomes2 = new LinkedList<String>();
+        nomes2.add("João");
+        nomes2.add("Maria");
+        nomes2.add("José");
+        nomes2.add("Pedro");
+        nomes2.add("Ana");
+
+        System.out.println(nomes2);
+        System.out.println("Tamanho do arraylist: " + nomes2.size());
+        System.out.println("Nome na posição 2: " + nomes2.get(2));
+        System.out.println("Nome na posição 3: " + nomes2.get(0));
+        //removendo um nome
+        nomes2.remove(0);
+        System.out.println("Nome na posição 0: " + nomes2.get(0));
+        //removendo todos os nomes
+        nomes2.clear();
+        System.out.println("Tamanho do LinkedList: " + nomes2.size());
+
+
     }
 }
