ae9d2cb1bb690a746d00045d857740f2c0338053
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-04-11 16:19:19 -0300

    Finalizando a primeirra parte

--- src/br/com/heranca/Funcionario.java
+++ src/br/com/heranca/Funcionario.java
@@ -7,39 +7,48 @@ abstract class Funcionario extends SistemaInterno {
     protected int senha;
     protected int numeroDeFuncionarios;
 
-
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getCpf() {
         return cpf;
     }
+
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
+
     public double getSalario() {
         return salario;
     }
+
     public void setSalario(double salario) {
         this.salario = salario;
     }
+
     public int getSenha() {
         return senha;
     }
+
     public void setSenha(int senha) {
         this.senha = senha;
     }
+
     public int getNumeroDeFuncionarios() {
         return numeroDeFuncionarios;
     }
+
     public void setNumeroDeFuncionarios(int numeroDeFuncionarios) {
         this.numeroDeFuncionarios = numeroDeFuncionarios;
     }
-    
-   abstract double getBonificacao();
+
+    abstract double getBonificacao();
+
     public boolean auntentica(int senha) {
         if (this.senha == senha) {
             System.out.println("Acesso permitido");
@@ -49,15 +58,16 @@ abstract class Funcionario extends SistemaInterno {
             return false;
         }
     }
+
     public void sacar(double valor) {
-      double Salario = this.getSalario();
+        double Salario = this.getSalario();
         if (valor <= Salario) {
             Salario -= valor;
             this.setSalario(Salario);
             System.out.println("Saque realizado com sucesso");
         } else {
             System.out.println("Saldo insuficiente");
-        }        
+        }
     }
-   
+
 }
